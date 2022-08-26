6bf86c76b7bf1724473e7b156a21bbd96067e620
Alex Merzlikin <alexmerzlikin@protonmail.com>
2022-08-26 18:14:53 +0200

    Setup a scene with naive approach modified to use multi plane sand object

---
+++ Assets/Scripts/Core/MultiPlane/DeformablePlanesComposite.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace Core.MultiPlane
+{
+    public class DeformablePlanesComposite : DeformablePlane
+    {
+        [SerializeField] private DeformablePlane[] _deformablePlanes;
+        private Bounds[] _bounds;
+        private Vector3 _positionToDeform;
+
+        private void Awake()
+        {
+            _bounds = new Bounds[_deformablePlanes.Length];
+            for (var i = 0; i < _deformablePlanes.Length; i++)
+            {
+                _bounds[i] = _deformablePlanes[i].GetComponent<Collider>().bounds;
+            }
+        }
+
+        public override void Deform(Vector3 positionToDeform)
+        {
+            _positionToDeform = positionToDeform;
+            for (var i = 0; i < _deformablePlanes.Length; i++)
+            {
+                var deformablePlane = _deformablePlanes[i];
+                var bounds = _bounds[i];
+                var sqrDistance = bounds.SqrDistance(_positionToDeform);
+                if (sqrDistance < 1f)
+                {
+                    deformablePlane.Deform(positionToDeform);
+                }
+            }
+        }
+    }
+}
